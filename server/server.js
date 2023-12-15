c56ea3e8ec216d59532920d6da1ec96f2cd4925f
Nefta007 <nperc001@ucr.edu>
2023-12-14 21:03:31 -0800

    changed server code

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const startApolloServer = async () => {
 	app.use(express.json())
 
 	// Serve up static assets
-	app.use('/images', express.static(path.join(__dirname, '../client/images')))
+	app.use('/images', express.static(path.join(__dirname, '../frontend/images')))
 
 	app.use(
 		'/graphql',
@@ -32,10 +32,10 @@ const startApolloServer = async () => {
 	)
 
 	if (process.env.NODE_ENV === 'production') {
-		app.use(express.static(path.join(__dirname, '../client/dist')))
+		app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 		app.get('*', (req, res) => {
-			res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+			res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 		})
 	}
 
